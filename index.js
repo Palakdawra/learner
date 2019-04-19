6e2cf94fa1147a43ba78b884f335e643c3992604
Palakdawra <professional.palak@gmail.com>
2019-04-19 09:25:22 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,13 @@
+function isNullOrEmpty(input) {
+// Returns true if the input is either undefined, null, or empty, false otherwise
+
+    return (input === undefined || input === null || input === '');
+
+}
+
+// Export to make the function available to other packages
+
+module.exports = isNullOrEmpty;
+var isNullOrEmpty = require('is-null-or-empty');
+console.log(isNullOrEmpty(""));
+console.log(isNullOrEmpty("Hello World")); // false
